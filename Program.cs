a8108288ff26e02b033fc28c4485571b5a280861
LZsolar <minecraftradioevil@gmail.com>
2022-12-03 14:39:04 +0700

    Finish

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Tree<string> tree = new Tree<string>();
+        tree.addRoot();
+
+        int countMember=0;
+        while(true){
+            string name = Console.ReadLine();
+            int node = int.Parse(Console.ReadLine());
+
+            tree.addToTree(name,node);
+
+            countMember++;
+            if(tree.GetLength()==countMember){break;}
+        }
+        string vacationName = Console.ReadLine();
+        Stack<string> boss = new Stack<string>();
+        bool isFind = false;
+        tree.FindVacation(tree.getRoot(),vacationName,ref boss,ref isFind);
+    }
+}
