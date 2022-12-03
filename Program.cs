edaa680e3769391a5c131289f5c595038a5cfb8e
LZsolar <minecraftradioevil@gmail.com>
2022-12-03 14:40:30 +0700

    Revert "Finish"
    This reverts commit a8108288ff26e02b033fc28c4485571b5a280861.

--- Program.cs
+++ Program.cs
@@ -1,23 +1,2 @@
-﻿class Program
-{
-    static void Main(string[] args)
-    {
-        Tree<string> tree = new Tree<string>();
-        tree.addRoot();
-
-        int countMember=0;
-        while(true){
-            string name = Console.ReadLine();
-            int node = int.Parse(Console.ReadLine());
-
-            tree.addToTree(name,node);
-
-            countMember++;
-            if(tree.GetLength()==countMember){break;}
-        }
-        string vacationName = Console.ReadLine();
-        Stack<string> boss = new Stack<string>();
-        bool isFind = false;
-        tree.FindVacation(tree.getRoot(),vacationName,ref boss,ref isFind);
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
